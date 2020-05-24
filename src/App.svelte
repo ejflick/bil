1b483d4d66e72018428cb54748bb29b8bec7e4fe
Eric Flick <flick.ej@gmail.com>
2020-05-24 12:34:52 +0800

    Bare bones setup with a little code.

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Bill from './Bill.svelte';
+	import Tenant from './Tenant.svelte';
+	
+	let bills = [
+		{
+			name: 'Gas',
+			period: {
+				start: new Date(2020, 0, 1),
+				end: new Date(2020, 1, 1)
+			},
+			amount: 30,
+		},
+		
+		{
+			name: 'Water',
+			period: {
+				start: new Date(2020, 3, 1),
+				end: new Date(2020, 4, 1)
+			},
+			amount: 30,
+		},
+	];
+	
+	let tenants = [
+		{name: 'Eric'},
+		{name: 'Person2'}
+	];
+</script>
+
+<style>
+	.parent {
+			display: flex;
+			align-items: top;
+	}
+
+	.column {
+		flex: 1
+	}
+</style>
+
+<div class="parent">
+	<div class="column">
+		{#each bills as bill}
+			<Bill {...bill} />
+		{:else}
+			<p>No bills found!</p>
+		{/each}
+	</div>
+	<div class="column">
+		{#each tenants as tenant}
+			<Tenant {...tenant} />
+		{:else}
+			<p>No roommates</p>
+		{/each}
+	</div>
+</div>
+
---
+++ src/Tenant.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let name;
+	//export let stayPeriod;
+</script>
+
+<h1>
+	{name}
+</h1>
